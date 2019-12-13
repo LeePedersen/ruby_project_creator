bce01242310da114a8cf7c680836eab522689a85
Student <example@example.com>
2019-12-12 16:30:11 -0800

    create project creator

---
+++ builder.rb
@@ -0,0 +1,16 @@
+puts "Project name?"
+name = gets.chomp
+puts `mkdir ../#{name}`
+puts `mkdir ../#{name}/lib ../#{name}/spec`
+gemfile_content = "source 'https://rubygems.org'\ngem 'rspec'\ngem 'pry'"
+File.write("../#{name}/Gemfile",gemfile_content)
+puts "Enter the class names below (separate by space)"
+classes = gets.chomp
+
+classes = classes.split(" ")
+classes.each do |theClass|
+  mainContent = "class #{theClass.capitalize}\nend"
+  specContent = "require '#{theClass.downcase}'\n\ndescribe('#{theClass.capitalize}') do\nend"
+  File.write("../#{name}/lib/#{theClass.downcase}.rb",mainContent)
+  File.write("../#{name}/spec/#{theClass.downcase}_spec.rb",specContent)
+end
